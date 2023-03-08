51e13a2634fc9ca0896d0e13f081f2be9d8c492e
Niklas <zhoie@icloud.com>
2023-03-09 02:12:15 +0800

    ads

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -82,6 +82,7 @@ export const PageHead: React.FC<
       <title>{title}</title>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4947882264558141"
      crossorigin="anonymous"></script>
+     
     </Head>
   )
 }
