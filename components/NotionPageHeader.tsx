8cf6ddce1abada421ea76a6369dc5b7ae37188b7
Niklas <zhoie@icloud.com>
2023-03-09 01:31:59 +0800

    ads

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -44,6 +44,8 @@ export const NotionPageHeader: React.FC<{
 
   return (
     <header className='notion-header'>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4947882264558141"
+     crossorigin="anonymous"></script>
       <div className='notion-nav-header'>
         <Breadcrumbs block={block} rootOnly={true} />
 
